1e1d8859e398f974ac63df7be100b48de238ca5a
dedge-ct <tuquerojedric@gmail.com>
2025-05-01 00:05:41 +0800

    Added document reader feature
    For java to read from a document and insert data into string variable

--- main.java
+++ main.java
@@ -1,6 +1,22 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
 public class main {
     public static void main(String args[]) {
         System.out.println("Jacob Las");
         System.out.println("Ken Chedrey Duculan");
+        try {
+            File documentOne = new File("Doc1.txt");
+            Scanner myReader = new Scanner(documentOne);
+            while (myReader.hasNextLine()) {
+                String documentOneText = myReader.nextLine();
+                System.out.println(documentOneText);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find document.");
+        }
     }
 }
