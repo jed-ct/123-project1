3c40e7f546932268330b24290908582c94e27031
dedge-ct <tuquerojedric@gmail.com>
2025-05-07 15:37:07 +0800

    Added comments

--- main.java
+++ main.java
@@ -1,19 +1,22 @@
+//Import necessary libraries
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Scanner;
 
 
 public class main {
+    //Converts a string into an array of words
     public static String[] convertToWordArray(String sentence) {
+        //remove punctuation and use space as separator
         return sentence.replaceAll("[^a-zA-Z ]", "").split(" ");
     }
 
-    //Function to print out the union of hashsets of docs
+    //Function to print out the union of hashsets
     public static void printUnionOfSets(ArrayList<HashSet<String>> setOfDocuments) {
         HashSet<String> result = new HashSet<>();
-        //Since hashsets ignore duplicate values, you can just add it
+        //Since hashsets ignore duplicate values, we can just add individual elements to it
         for (HashSet<String> hashset : setOfDocuments) {
             for (String documents : hashset) {
                 result.add(documents);
@@ -21,20 +24,21 @@ public class main {
         }
         printSet(result);
     }
-    //Function to print out the union of hashsets of docs
+    //Function to print out the intersection of hashsets of docs
     public static void printIntersectionOfSets(ArrayList<HashSet<String>> setOfDocuments) {
         //Create hashset of all documents
         HashSet<String> neededDocuments = new HashSet<>(Arrays.asList("Doc1", "Doc2", "Doc3"));
         for (HashSet<String> hashset : setOfDocuments) {
-            //Intersect this hashset of all documents to all sets
+            //Intersect this hashset of document collection to all sets using .retainAll
             neededDocuments.retainAll(hashset);
         }
+        //Print error if no common documents
         if (neededDocuments.isEmpty()) {
             System.out.println("No common documents found.");
         }
         printSet(neededDocuments);
     }
-
+    //Prints hashset
     public static void printSet(HashSet<String> set) {
         if (set == null) {
             System.out.println("Cannot find word in document.");
@@ -45,6 +49,7 @@ public class main {
             }
         }
     }
+    //Retrieves the location of selected word/s across all documents
     public static void retrieveWordLocations(String userQuery, HashMap<String,HashSet<String>> wordMap) {
         ArrayList<HashSet<String>> arrayOfDocumentSets = new ArrayList<>();
         if (userQuery.contains("OR")) {
@@ -78,7 +83,7 @@ public class main {
             printSet(wordMap.get(userQuery));
         }
     }
-
+    //Adds individual words to hashmap along with their corresponding locations
     public static void addToWordMap(String[] wordArray, HashMap<String, HashSet<String>> wordMap,String documentName) {
         for (String word : wordArray) {
             if (wordMap.get(word) != null) {
